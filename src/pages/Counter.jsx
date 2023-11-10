4614ee706046332ba75e90ffe3550ce773ebc6db
Jack Barnett <jackbarnett@Jacks-MacBook-Pro-2.local>
2023-11-10 18:13:32 +0000

    initial commit

---
+++ src/pages/Counter.jsx
@@ -0,0 +1,17 @@
+import {useState, useEffect} from 'react'
+
+export default function Counter() {
+    const [counter, setCounter] = useState(0)
+
+    function changeCount(value){
+        setCounter(counter + value)
+    }   
+   
+  return (
+    <div className="bg-gray-700">
+        <button onClick={() => changeCount(-1)}>-</button>
+        <span>{counter}</span>
+        <button onClick={() => changeCount(1)}>+</button>
+    </div>
+  )
+}
